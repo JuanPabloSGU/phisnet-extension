e1935ce673b057b1a8722a04151c30c7e1d89bdc
andreevdan <andreevdan@gmail.com>
2024-11-08 09:08:16 -0500

    Revert "Merge with BackUp"
    This reverts commit 54c4807fb7ce782db4b9783c1a54802efd3b1c8f.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         background: './src/background.ts',
         content: './src/content.ts',
         popup: './src/popup.ts',
-        home: './src/home.ts',
-        settings: './src/settings.ts'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -25,13 +22,6 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/manifest.json', to: 'manifest.json' },
-            ]
-        })
-    ],
     mode: 'production'
 };
 
