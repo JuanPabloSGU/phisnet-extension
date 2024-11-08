4e6443dd88b65d34dd60c335645d5951e7d447f2
andreevdan <andreevdan@gmail.com>
2024-11-08 09:19:18 -0500

    SEG-4 Ticket

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,11 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed');
+    fetch("http://localhost:5000/api/v1/hello_world")
+        .then(response => response.json())
+        .then(data => {
+            console.log('Data from server:', data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 });
+
